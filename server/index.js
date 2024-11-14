50caf9662c9c91015b61177b7fc30f5f84e56b4b
Bharath S <bharathks005@gmail.com>
2024-11-14 13:53:38 +0530

    test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const ngrok = require('@ngrok/ngrok');
 const path = require('path');
 require('dotenv').config();
 
